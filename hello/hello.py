1b1f9894a26e7c76e3b9873278206cb506e5b365
Delvinvjose <delvinvjose@gmail.com>
2024-11-30 20:35:46 +0000

    hello.py

--- hello/hello.py
+++ hello/hello.py
@@ -1,17 +1,17 @@
-# from flask import Flask
-# app = Flask(__name__)
-
-# @app.route("/")  # URL leading to method
-# def hello():  # Name of the method
-#     return "Hello World!"  # indent this line
-
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=8080, ssl_context=('cert.pem', 'privkey.pem'))
-
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return("Hello World!")
+
+@app.route("/")  # URL leading to method
+def hello():  # Name of the method
+    return "Hello World!"  # indent this line
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080')
+    app.run(host='0.0.0.0', port=8080, ssl_context=('cert.pem', 'privkey.pem'))
+
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route("/")
+# def hello():
+#     return("Hello World!")
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port='8080')
