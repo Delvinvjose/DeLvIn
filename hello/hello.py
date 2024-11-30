907964cba7d53156a4643aa2aefc261bd8f86011
Delvinvjose <delvinvjose@gmail.com>
2024-11-30 19:49:17 +0000

    Update hello.py

--- hello/hello.py
+++ hello/hello.py
@@ -1,11 +1,18 @@
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route("/")  # URL leading to method
+# def hello():  # Name of the method
+#     return "Hello World!"  # indent this line
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=8080, ssl_context=('cert.pem', 'privkey.pem'))
+
 from flask import Flask
 app = Flask(__name__)
-
-@app.route("/")  # URL leading to method
-def hello():  # Name of the method
-    return "Hello World!"  # indent this line
-
+@app.route("/")
+def hello():
+    return("Hello World!")
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, ssl_context=('cert.pem', 'privkey.pem'))
-
+    app.run(host='0.0.0.0', port='8080')
 
